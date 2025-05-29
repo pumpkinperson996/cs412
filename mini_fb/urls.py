3aa108204be2b176d836e3cbc32ec2fb401f798a
pumpkinperson996 <david996@bu.edu>
2025-05-29 14:41:32 -0400

    complete a3

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,7 +1,26 @@
+"""
+urls.py - URL Configuration for Mini Facebook Application
+
+This module defines the URL patterns for the mini_fb application.
+It maps URL paths to their corresponding view classes.
+
+URL Patterns:
+1. '' (empty string) - Root URL that displays all profiles
+   - View: ShowAllProfilesView
+   - Name: 'show_all_profiles'
+   - Template: show_all_profiles.html
+   
+2. 'profile/<int:pk>' - Individual profile detail page
+   - View: ShowProfilePageView
+   - Name: 'show_profile'
+   - Template: show_profile.html
+   - Parameters: pk (primary key of the profile)
+"""
 from django.urls import path
-from .views import ShowAllProfilesView # our view class definition 
+from .views import ShowAllProfilesView, ShowProfilePageView
 
 urlpatterns = [
     # map the URL (empty string) to the view
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'), # generic class-based view
+    path('profile/<int:pk>', ShowProfilePageView.as_view(), name='show_profile')
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,8 +1,26 @@
+"""
+views.py - View Classes for Mini Facebook Application
+
+This module contains the view classes that handle HTTP requests and responses
+for the mini_fb application. It uses Django's generic class-based views for
+common patterns like listing and displaying details.
+
+Views:
+1. ShowAllProfilesView - Displays all user profiles
+2. ShowProfilePageView - Displays a single profile's details
+
+
+Author: Shuwei Zhu
+Date: 5/29/2025
+"""
+
 from django.shortcuts import render
 
 # Create your views here.
 from .models import Profile
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+
+
 
 class ShowAllProfilesView(ListView):
     '''Create a subclass of ListView to display all Profile.'''
@@ -10,3 +28,9 @@ class ShowAllProfilesView(ListView):
     model = Profile # retrieve objects of type Profile from the database
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles' # how to find the data in the template file
+    
+class ShowProfilePageView(DetailView):
+    '''Show the details for one Profile.'''
+    model = Profile
+    template_name = 'mini_fb/show_profile.html' 
+    context_object_name = 'profiles'
